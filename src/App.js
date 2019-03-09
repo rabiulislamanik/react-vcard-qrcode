384cc2b57a2b001fe84ff880471a8ebafe68b160
Rabiul Islam Anik <rabiul.islam2676@gmail.com>
2019-03-10 01:40:34 +0600

    implemented the download function with fetch. removed xhr implementation

--- src/App.js
+++ src/App.js
@@ -15,20 +15,17 @@ class App extends Component {
     email: ""
   };
   downloadQRCode = (url, fileName) => {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.responseType = "blob";
-    xhr.onload = function() {
-      var urlCreator = window.URL || window.webkitURL;
-      var imageUrl = urlCreator.createObjectURL(this.response);
-      var tag = document.createElement("a");
-      tag.href = imageUrl;
-      tag.download = fileName;
-      document.body.appendChild(tag);
-      tag.click();
-      document.body.removeChild(tag);
-    };
-    xhr.send();
+    fetch(url, {})
+      .then(response => response.blob())
+      .then(blob => URL.createObjectURL(blob))
+      .then(url => {
+        const tag = document.createElement("a");
+        tag.href = url;
+        tag.download = fileName;
+        document.body.appendChild(tag);
+        tag.click();
+        document.body.removeChild(tag);
+      });
   };
 
   handleTextChange = e => {
